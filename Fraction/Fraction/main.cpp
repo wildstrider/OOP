802d1fc7b4a3f7944e770f410c524fbb4d2554af
wildstrider2605@gmail.com <wildstrider2605@gmail.com>
2021-06-06 12:17:19 +0400

    done

---
+++ Fraction/Fraction/main.cpp
@@ -0,0 +1,173 @@
+﻿#include <iostream>
+using namespace std;
+#define tab "\t"
+class Fraction;
+
+Fraction operator+(Fraction& fraction_1, Fraction& fraction_2);
+Fraction operator-(Fraction& fraction_1, Fraction& fraction_2);
+Fraction operator*(Fraction& fraction_1, Fraction& fraction_2);
+Fraction operator/(Fraction& fraction_1, Fraction& fraction_2);
+
+class Fraction
+{
+	int numerator;
+    int denominator;	
+public:
+	int GetNumerator() { return numerator; }
+	int GetDenominator() { return denominator; }
+	void SetNumenator(int numenator) { this->numerator = numenator; }
+	void SetDenominator(int denominator) { this->denominator = denominator; }
+	void Print() 
+	{ cout << this->GetNumerator() << '/' << this->GetDenominator() << endl; }
+	Fraction& operator++()//++Fraction
+	{
+		Fraction fracTemp;
+		fracTemp.SetNumenator(this-> GetDenominator());
+		fracTemp.SetDenominator(this-> GetDenominator());
+		return *this = *this + fracTemp;
+	}
+	Fraction operator++(int)//Fraction++ 
+	{
+		Fraction old = *this;
+		++*this;
+		return old;
+	}
+	Fraction& operator--()//--Fraction
+	{
+		Fraction fracTemp;
+		fracTemp.SetNumenator(this->GetDenominator());
+		fracTemp.SetDenominator(this->GetDenominator());
+		return *this = *this - fracTemp;
+	}
+	Fraction operator--(int)//--Fraction
+	{
+		Fraction old = *this;
+		--* this;
+		return old;
+	}
+	Fraction& operator+=(Fraction& other)
+	{
+		*this = *this + other;
+		return *this;
+	}
+	Fraction& operator-=(Fraction& other)
+	{
+		*this = *this - other;
+		return *this;
+	}
+	Fraction& operator*=(Fraction& other)
+	{
+		*this = *this * other;
+		return *this;
+	}
+	Fraction& operator/=(Fraction& other)
+	{
+		*this = *this / other;
+		return *this;
+	}
+	Fraction(int numerator = 1, int denominator = 2)
+	{
+		this->numerator = numerator;
+		this->denominator = denominator;
+	}
+	~Fraction() {}
+
+	friend istream& operator>>(istream& in, Fraction& other);
+	friend ostream& operator<<(ostream& out, Fraction& other);
+};
+
+void FractionReduction(Fraction& other)
+{
+	for (int i = other.GetNumerator(); i > 0; i--) {
+
+		if (other.GetNumerator() % i == 0 && other.GetDenominator() % i == 0) {
+			other.SetNumenator(other.GetNumerator() / i);
+			other.SetDenominator(other.GetDenominator() / i);
+			break;
+		}
+	}
+}
+ostream& operator<<(ostream& out, Fraction& other) { return out << other.numerator << '/' << other.denominator; }
+istream& operator>>(istream& in, Fraction& other)
+{
+	cout << "Введите числитель: ";
+	in >> other.numerator;
+	cout << "Введите знаменатель: ";
+	in >> other.denominator;
+	return in;
+}
+Fraction operator+(Fraction& fraction_1, Fraction& fraction_2)
+{
+	Fraction fracTemp;
+	if (fraction_1.GetDenominator() == fraction_2.GetDenominator())
+	{
+		fracTemp.SetNumenator(fraction_1.GetNumerator() + fraction_2.GetNumerator());
+		fracTemp.SetDenominator(fraction_1.GetDenominator());
+	}
+	else
+	{
+		fracTemp.SetNumenator((fraction_1.GetNumerator() * fraction_2.GetDenominator()) + (fraction_2.GetNumerator() * fraction_1.GetDenominator()));
+		fracTemp.SetDenominator(fraction_1.GetDenominator() * fraction_2.GetDenominator());
+	}
+	
+     FractionReduction(fracTemp);
+	return fracTemp;
+}
+Fraction operator-(Fraction& fraction_1, Fraction& fraction_2)
+{
+	Fraction fracTemp;
+	if (fraction_1.GetDenominator() == fraction_2.GetDenominator())
+	{
+		fracTemp.SetNumenator(fraction_1.GetNumerator() - fraction_2.GetNumerator());
+		fracTemp.SetDenominator(fraction_1.GetDenominator());
+	}
+	else
+	{
+		fracTemp.SetNumenator((fraction_1.GetNumerator() * fraction_2.GetDenominator()) - (fraction_2.GetNumerator() * fraction_1.GetDenominator()));
+		fracTemp.SetDenominator(fraction_1.GetDenominator() * fraction_2.GetDenominator());
+	}
+	FractionReduction(fracTemp);
+	return fracTemp;
+}
+Fraction operator*(Fraction & fraction_1, Fraction& fraction_2)
+{
+	Fraction fracTemp;
+	fracTemp.SetNumenator(fraction_1.GetNumerator() * fraction_2.GetNumerator());
+	fracTemp.SetDenominator(fraction_1.GetDenominator() * fraction_2.GetDenominator());
+	FractionReduction(fracTemp);
+	return fracTemp;
+}
+Fraction operator/(Fraction& fraction_1, Fraction& fraction_2)
+{
+	Fraction fracTemp;
+	fracTemp.SetNumenator(fraction_1.GetNumerator() * fraction_2.GetDenominator());
+	fracTemp.SetDenominator(fraction_1.GetDenominator() * fraction_2.GetNumerator());
+	FractionReduction(fracTemp);
+	return fracTemp;
+}
+bool operator==(Fraction& fraction_1, Fraction& fraction_2)
+{ return fraction_1.GetNumerator() == fraction_2.GetNumerator() && fraction_1.GetDenominator() == fraction_2.GetDenominator(); }
+bool operator!=(Fraction& fraction_1, Fraction& fraction_2)
+{ return !(fraction_1.GetNumerator() == fraction_2.GetNumerator() && fraction_1.GetDenominator() == fraction_2.GetDenominator()); }
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	Fraction fraction_1, fraction_2, fraction_3;
+	cout << "fraction_1 " << endl;
+	cin >> fraction_1;
+	cout << "fraction_2" << endl;
+	cin >> fraction_2;
+	cout << "Выберите действие +-*/ " << endl; char sign; cin >> sign;
+
+	switch (sign)
+	{
+	case '+': fraction_3 = fraction_1 + fraction_2; break;
+	case '-': fraction_3 = fraction_1 - fraction_2; break;
+	case '*': fraction_3 = fraction_1 * fraction_2; break;
+	case '/': fraction_3 = fraction_1 / fraction_2; break;
+	}
+	cout << fraction_1 << sign << fraction_2 << " = " << fraction_3 << endl;
+	return 0;
+}
+
+	
--- init/init/main.cpp
+++ init/init/main.cpp
@@ -2,24 +2,27 @@
 #include <cmath>
 using namespace std;
 #define tab "\t"
-//#define CONSTRUCTOR
-class Point 
+#define hello(arg) "hello "#arg
+
+#define CONSTRUCTOR
+#define delimeter "\n====================================================\n"
+class Point
 {
 private:
 	double x;
 	double y;
 public:
-	double get_x() const{ return x; }
-	double get_y() const{ return y; }
-	void set_x(double x){ this->x = x; }
-	void set_y(double y){ this->y = y; }
+	double get_x() const { return x; }
+	double get_y() const { return y; }
+	void set_x(double x) { this->x = x; }
+	void set_y(double y) { this->y = y; }
 
 #ifdef CONSTRUCTOR
-	Point()
+	/*Point()
 	{
 		x = y = 0;
 		cout << "defaultconstructor: \t" << this << endl;
-	}
+	}*/
 	Point(double x = 0, double y = 0)
 	{
 		this->x = x;
@@ -35,22 +38,52 @@ public:
 	~Point()
 	{
 		cout << "destructor: \t" << this << endl;
-
 	}
 #endif 
-
+	Point& operator= (const Point& other) {
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyAssigment:\t" << this << endl;
+		return *this;
+	}
+	/*Point operator+(const Point& other) const
+	{
+		Point result;
+		result.x = this->x + other.x;
+		result.y = this->y + other.y;
+		cout << "operator+:\t" << this << endl;
+		return result;
+	}*/
+	Point& operator++()//++c
+	{
+		this->x++;
+		this->y++;
+		cout << "increment:\t" << this << endl;
+		return *this;
+	}
+	Point operator++(int)//c++
+	{
+		Point old = *this;
+		this->y++;
+		this->x++;
+		cout << "suffix incremrnt" << endl;
+		return old;
+	}
 	void Print()
 	{
 		cout << "X = " << x << tab << "Y = " << y << endl;
 	}
-	double DistanceToPoint()
+	double DistanceToPoint(const Point &other) const
 	{
-		 return sqrt(pow(x, 2) + pow(y, 2));
+		return sqrt(pow((other.get_x() - this->get_x()), 2) + pow((other.get_y() - this->get_y()), 2));
 	}
 };
 
-double DistanceBetweenPoints(Point A, Point B);
+double DistanceBetweenPoints(const Point &A, const Point &B);
+Point operator+(const Point& left, const Point& right);
 //#define STRICT	
+//#define distance_and_constructors
+//#define ASSIGMENT_CHECK
 int main()
 {
 	setlocale(LC_ALL, "ru");
@@ -64,22 +97,61 @@ int main()
 	cout << pA->x << tab << pA->y << endl;
 #endif 
 
-	Point A; 
-	A.set_x(-1);
-	A.set_y(3);
+#ifdef distance_and_constructors
+	Point A(1, 3);
+	//A.set_x(1);
+	//A.set_y(3);
 	//cout << A.get_x() << tab << A.get_y() << endl;
-	
-	Point B;
-	B.set_x(6);
-	B.set_y(2);
+
+	Point B(2, 6);
 	//B.Print();
-	//Point C = A; // copyconstructor
-	cout << "Расстояние до точки A - " << B.DistanceToPoint() << endl;
-	cout << "Расстояние от точки A до точки В - " << DistanceBetweenPoints(A,B) << endl;
-	Point g;
+	Point C = A; // copyconstructor
+
+	cout << delimeter << endl;
+	cout << "Расстояние до точки B - " << A.DistanceToPoint(B) << endl;
+	cout << "Расстояние до точки A - " << B.DistanceToPoint(A) << endl;
+	cout << "Расстояние от точки A до точки В - " << DistanceBetweenPoints(A, B) << endl;
+	cout << "Расстояние от точки A до точки В - " << DistanceBetweenPoints(B, A) << endl;
+	cout << delimeter << endl;
+	//cout << delimeter << endl;
+	Point E = A;
+	Point F;
+	F = B;
+	cout << delimeter << endl;
+#endif 
+#ifdef ASSIGMENT_CHECK
+	Point A, B, C;
+	cout << delimeter << endl;
+	A = B = C = Point(2.3, 4.5);
+	cout << delimeter << endl;
+	A.Print();
+	B.Print();
+	C.Print();
+	cout << delimeter << endl;
+#endif // ASSIGMENT_CHECK
+
+	//cout << hello(guy) << endl;
+	Point A(2.3, 4.5);
+	Point B(2.7, 3.14);
+	Point C = A + B;
+	C.Print();
+	C++;
+	C.Print();
+	++C;
+	C.Print();
+
 	return 0;
 }
-double DistanceBetweenPoints(Point A, Point B)
+double DistanceBetweenPoints(const Point &A, const Point &B)
 {
 	return sqrt(pow((B.get_x() - A.get_x()), 2) + pow((B.get_y() - A.get_y()), 2));
+}
+
+Point operator+(const Point& left, const Point& right)
+{
+	Point result;
+	result.set_x(left.get_x() + right.get_x());
+	result.set_y(left.get_y() + right.get_y());
+	cout << "global plus" << endl;
+	return result;
 }
