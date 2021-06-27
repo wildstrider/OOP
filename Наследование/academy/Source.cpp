6819678f848f87f5a3f27cc2dc92a84a9be86061
wildstrider2605@gmail.com <wildstrider2605@gmail.com>
2021-06-27 12:21:38 +0400

    done

--- Наследование/academy/Source.cpp
+++ Наследование/academy/Source.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#define delimiter "\n=====================================================\n"
 using namespace std;
 
 class Human
@@ -13,7 +14,7 @@ public:
 	void Set_last_name(const string& last_name) { this->last_name = last_name; }
 	void Set_first_name(const string& first_name) { this->first_name = first_name; }
 	void Set_age(unsigned int age) { this->age = age; }
-	void Print() { cout << last_name << " " << first_name << " " << age << "лет." << endl; }
+	virtual void Print() const { cout << last_name << " " << first_name << " " << age << "лет." << endl; }
 	Human(const string& last_name, const string& first_name, unsigned int age)
 	{
 		Set_last_name(last_name);
@@ -21,8 +22,11 @@ public:
 		Set_age(age);
 		cout << "Hconstructor: " << this << endl;
 	}
-	~Human() { cout << "Hdestructor: " << this << endl; }
+	virtual ~Human() { cout << "Hdestructor: " << this << endl; }
+	virtual void Print(ostream& os)	const { os << Get_last_name() << " " << Get_first_name() << " " << Get_age() << endl; }
+	friend ostream& operator<<(ostream& os, const Human& obj); 
 };
+
 
 class Student :public Human
 {
@@ -32,7 +36,7 @@ class Student :public Human
 public:
 	const string& Get_speciality() const { return speciality; }
 	const string& Get_group() const { return group; }
-	double Get_ratihg() const { return rating; }
+	double Get_rating() const { return rating; }
 	void Set_speciality(const string& speciality) { this->speciality = speciality; }
 	void Set_group(const string& group) { this->group = group; }
 	void Set_rating(double rating) { this->rating = rating; }
@@ -52,7 +56,14 @@ public:
 		cout << "Sconstructor: " << this << endl;
 	}
 	~Student() { cout << "Sdestructor: " << this << endl; }
+	void Print(ostream& os) const
+	{
+		Human::Print();
+		os << "специальность: " << Get_speciality() << " группа: " << Get_group() << " успеваемость: " << Get_rating() << endl;
+	}
 };
+
+
 
 class Teacher :public Human
 {
@@ -68,6 +79,11 @@ public:
 		Human::Print();
 		cout << "специальность: " << speciality << " опыт преподавания: " << expirience << " лет." << endl;
 	}
+	 void Print(ostream& os) const
+	{
+		Human::Print();
+		os <<  "специальность: " << Get_speciality() << " опыт преподавания: " << Get_expirience() << " лет." << endl;
+	}
 	Teacher(const string& last_name, const string& first_name, unsigned int age,
 		const string& speciality, unsigned int expirience) :Human(last_name, first_name, age)
 	{
@@ -78,29 +94,67 @@ public:
 	~Teacher() { cout << "Tdestructor: " << this << endl; }
 };
 
+
 class graduate_student :public Student
 {
 	string project;
 public:
-	const string get_project() const { return project; }
-	void set_project(const string project) { this->project = project; }
+	const string& get_project() const { return project; }
+	void set_project(const string& project) { this->project = project; }
 	graduate_student(const string& last_name, const string& first_name, unsigned int age,
 		const string& speciality, const string& group, double rating, const string& project)
-		: Student(last_name, first_name, age, speciality, group, rating) { set_project(project); }
+		: Student(last_name, first_name, age, speciality, group, rating) {
+		set_project(project);
+		cout << "g_sConstructor: " << this << endl;
+	}
 	void print() { Student::Print(); cout << " Название проекта: " << get_project() << endl; }
-	~graduate_student() {}
+    void Print(ostream& os) { Student::Print(); os << " Название проекта: " << get_project() << endl; }
+	~graduate_student() { cout << "g_sDestructor: " << this << endl; }
 };
 
+ostream& operator<<(ostream& os, const Human& obj) { obj.Print(os); return os; }
 int main()
 {
 	setlocale(LC_ALL, "ru");
 	//Human human("Тупенко", "Василий", 18);
 	//human.Print();
-	Student stud("Тупенко", "Василий", 18, "РПО", "BV-123", 43.4);
+	/*Student stud("Тупенко", "Василий", 18, "РПО", "BV-123", 43.4);
 	stud.Print();
 	Teacher Albert("Einstein", "Albert", 45, "Astrophisics", 120);
 	Albert.Print();
 	graduate_student gr_std("Тупенко", "Василий", 18, "РПО", "BV-123", 43.4, "морской бой");
-	gr_std.print();
+	gr_std.print();*/
+	Human* group[] =
+	{
+	 new Student("Васильев", "Александр", 23, "РПО", "ПВ-01", 90),
+	 new graduate_student("Васильев", "Александр", 23, "РПО", "ПВ-01", 90, "морской бой"),
+	 new Student("Васильева", "Маргарита", 25, "РПО", "ПВ-01", 90),
+	 new Teacher("Ковтун", "Олег", 36, "Разработка приложение С++", 6),
+	 new Student("Ивлев", "Александр", 29, "РПО", "ПВ-01", 95),
+	 new Student("Рахманин", "Николай", 31, "РПО", "ПВ-01", 98),
+	 new Teacher("Романов", "Андрей", 30, "HardwarePC", 10),
+	 new Student("Нусс", "Дмитрий", 31, "РПО", "ПВ-01", 100),
+	 new Student("Борн", "Евгений", 35, "РПО", "ПВ-01", 99)
+	};
+
+	for (int i = 0; i < sizeof(group) / sizeof(Human*); i++)
+	{
+		////group[i]->Print();
+		//if(typeid(*group[i]) == typeid(Student))
+		//{
+		// cout << *dynamic_cast<Student*>(group[i]) << endl;
+		//}
+		//if (typeid(*group[i]) == typeid(Teacher))
+		//{
+		//	cout << *dynamic_cast<Teacher*>(group[i]) << endl;
+		//}
+		////cout << typeid(*group[i]).name() << endl;
+		cout << *group[i];
+		cout << delimiter << endl;
+	}
+	graduate_student D("Васильев", "Александр", 23, "РПО", "ПВ-01", 90, "морской бой");
+	D.print();
+
+	for (int i = 0; i < sizeof(group) / sizeof(Human*); delete group[i], i++);
 	return 0;
 }
